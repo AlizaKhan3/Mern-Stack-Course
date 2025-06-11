e4d3a2dff6da6261bbf506af4bb2fa72003f09ae
Aliza Khan <alizaadi2@gmail.com>
2025-06-11 21:47:40 +0500

    use of map method-Array

---
+++ src/components/renderList.jsx
@@ -0,0 +1,34 @@
+function RenderList() {
+    let arr = [1, 8, 3, 4, 5, 100, 3000, 19110];
+    // let arr = [1, 8, 3];
+
+    let rollno = [17, 18, 19, 20, 21, 22, 23, 24, 25];
+    return (
+        <div>
+            <ul>
+                {
+                    arr.map((value, index) => {
+                        console.log(value, index);
+
+                        return (
+                            <li key={index}>{value}</li>
+                        )
+                    })
+                }
+            </ul>
+            <div>
+                {
+                    rollno.map((val, ind) => {
+                        return (
+                            <h1 key={ind}> {val} </h1>
+                        )
+                    })
+                }
+            </div>
+
+        </div>
+
+    )
+}
+
+export default RenderList;
