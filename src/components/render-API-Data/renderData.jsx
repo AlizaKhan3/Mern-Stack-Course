391488ef9a30ecff2ef0d9af068fc7769b6e46ce
Aliza Khan <alizaadi2@gmail.com>
2025-06-15 18:02:52 +0500

    Category Filter feature added

--- src/components/render-API-Data/renderData.jsx
+++ src/components/render-API-Data/renderData.jsx
@@ -1,21 +1,35 @@
 import ProductCard from "./card";
-import { cardDataArray } from "../../utils/data";
-// import { categoryArray } from "../../utils/data";
+import { productDataArray } from "../../utils/data";
 import DataCategory from "./Category";
+import { useState } from "react";
+
 const RenderData = () => {
-   
+    const [productList, setProductList] = useState(productDataArray)
+
+    const updateProductList = (e) => {
+        const selectedCategory = e.target.value;
+        console.log(selectedCategory)
+        let filteredData;
+
+        if (selectedCategory === "") {
+            filteredData = productDataArray;
+        } else {
+            filteredData = productDataArray.filter((product) =>  product.category.name === selectedCategory);
+            setProductList(filteredData)
+        }
+    }
     return (
         <div>
-            <DataCategory/>
-                   <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 bg-blue-200 p-6">
-            {
-                cardDataArray.map((object, index) => {
-                    return(
-                        <ProductCard key={index} ObjectData={object}/>
-                    )
-                })
-            }
-        </div> 
+            <DataCategory handleCategory={updateProductList} />
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 bg-blue-200 p-6">
+                {
+                    productList.map((object, index) => {
+                        return (
+                            <ProductCard key={index} ObjectData={object} />
+                        )
+                    })
+                }
+            </div>
         </div>
 
     )
