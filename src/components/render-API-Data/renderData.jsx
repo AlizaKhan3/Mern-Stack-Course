9aec2de575ebf77e8396a7add87d188375ac1ae5
Aliza Khan <alizaadi2@gmail.com>
2025-06-16 01:06:34 +0500

    Added Title based product filter feature

--- src/components/render-API-Data/renderData.jsx
+++ src/components/render-API-Data/renderData.jsx
@@ -2,6 +2,7 @@ import ProductCard from "./card";
 import { productDataArray } from "../../utils/data";
 import DataCategory from "./Category";
 import { useState } from "react";
+import TitleFilter from "./titleFilter";
 
 const RenderData = () => {
     const [productList, setProductList] = useState(productDataArray)
@@ -14,25 +15,51 @@ const RenderData = () => {
         if (selectedCategory === "") {
             filteredData = productDataArray;
         } else {
-            filteredData = productDataArray.filter((product) =>  product.category.name === selectedCategory);
+            filteredData = productDataArray.filter((product) => product.category.name === selectedCategory);
             setProductList(filteredData)
         }
     }
-    return (
-        <div>
-            <DataCategory handleCategory={updateProductList} />
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 bg-blue-200 p-6">
-                {
-                    productList.map((object, index) => {
-                        return (
-                            <ProductCard key={index} ObjectData={object} />
-                        )
-                    })
-                }
-            </div>
-        </div>
 
-    )
+    // const [titleList, setTitleList] = useState(productDataArray);
+    // const productTitles = productDataArray.filter((product) => product.title);
+    // console.log(productTitles)
+    const handleItem = (event) => {
+        const searchedItemValue = event.target.value;
+        console.log(searchedItemValue)
+
+        let filteredItem;
+
+        if (searchedItemValue === "") {
+            setProductList(productDataArray);
+        }
+        else {
+            filteredItem = productDataArray.filter((product) =>
+                product.title.toLowerCase().includes(searchedItemValue.toLowerCase())
+            );
+            setProductList(filteredItem);
+        }
+        console.log(filteredItem);
+        // console.log(searchedItemValue);
+    }
+
+return (
+    <div>
+        <div className="container flex m-4 gap-6">
+            <DataCategory handleCategory={updateProductList} />
+            <TitleFilter handleSearchedItem={handleItem} />
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 bg-blue-200 p-6">
+            {
+                productList.map((object, index) => {
+                    return (
+                        <ProductCard key={index} ObjectData={object} />
+                    )
+                })
+            }
+        </div>
+    </div>
+
+)
 }
 
 export default RenderData;
